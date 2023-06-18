94606824f958b28e11e3a691a517272f3318e530
Sujon Ali <mimobaydullah@gmail.com>
2023-06-18 09:41:20 +0600

    Created Model Controller API-Route Request ect

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Review;
+use App\Http\Requests\StoreReviewRequest;
+use App\Http\Requests\UpdateReviewRequest;
+
+class ReviewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreReviewRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Review $review)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateReviewRequest $request, Review $review)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Review $review)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +19,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::ApiResource('products',ProductController::class);
+Route::group(['prefix' => 'products'],function (){
+Route::ApiResource('/{product}/reviews',ReviewController::class);
 });
